883b479dd8289b5e8ed7c80e7f76a2a23bb40103
JoeQuispe2004 <142689599+JoeQuispe2004@users.noreply.github.com>
2023-11-11 10:14:57 -0500

    Add files via upload

---
+++ src/appregistroincidencias/AppRegistroIncidencias.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package appregistroincidencias;
+
+import Ventanas.InicioSesion;
+
+/**
+ *
+ * @author USER
+ */
+public class AppRegistroIncidencias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InicioSesion iniciodesesion=new InicioSesion();
+        iniciodesesion.setLocationRelativeTo(null);
+         iniciodesesion.setVisible(true);
+    }
+    
+}
